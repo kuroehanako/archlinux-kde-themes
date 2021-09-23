3d65ff0091b487015e3932b5b5f5d59dc0f5ed8a
KuroeHanako <78864678+kuroehanako@users.noreply.github.com>
2021-09-23 18:21:46 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,86 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+SRC_DIR=$(cd $(dirname $0) && pwd)
+ROOT_UID=0
 
-gh_repo="archlinux-kde-themes"
-gh_desc="Archlinux with KDE"
-
-cat <<- EOF
-
-  Welcome!
-
-  $gh_desc
-  https://github.com/kuroehanako/$gh_repo
-
-EOF
-
-: "${PREFIX:=/usr}"
-: "${TAG:=main}"
-: "${uninstall:=false}"
-
-_msg() {
-    echo "=>" "$@" >&2
-}
-
-_rm() {
-    # removes parent directories if empty
-    sudo rm -rf "$1"
-    sudo rmdir -p "$(dirname "$1")" 2>/dev/null || true
-}
-
-_download() {
-    _msg "Getting the latest version from GitHub ..."
-    wget -O "$temp_file" \
-        "https://github.com/kuroehanako/$gh_repo/archive/$TAG.tar.gz"
-    _msg "Unpacking archive ..."
-    tar -xzf "$temp_file" -C "$temp_dir"
-}
-
-_uninstall() {
-    _msg "Deleting..."
-    _rm "$PREFIX/share/plasma/desktoptheme/archlinux"
-	_rm "$PREFIX/share/plasma/desktoptheme/archlinux-dark"
-	_rm "$PREFIX/share/plasma/look-and-feel/org.archlinux.kdedark.desktop"
-	_rm "$PREFIX/share/plasma/look-and-feel/org.archlinux.kdedefault.desktop"
-	_rm "$PREFIX/share/plasma/look-and-feel/org.archlinux.kdelight.desktop"
-	_rm "$PREFIX/share/wallpapers/archbtw"
-	_rm "$PREFIX/share/wallpapers/archwave"
-	_rm "$PREFIX/share/wallpapers/archwaveinv"
-	_rm "$PREFIX/share/wallpapers/awesome"
-	_rm "$PREFIX/share/wallpapers/conference"
-	_rm "$PREFIX/share/wallpapers/geolanes"
-	_rm "$PREFIX/share/wallpapers/geowaves"
-	_rm "$PREFIX/share/wallpapers/gritty"
-	_rm "$PREFIX/share/wallpapers/landscape"
-	_rm "$PREFIX/share/wallpapers/lone"
-	_rm "$PREFIX/share/wallpapers/mountain"
-	_rm "$PREFIX/share/wallpapers/reflected"
-	_rm "$PREFIX/share/wallpapers/simple"
-	_rm "$PREFIX/share/wallpapers/small"
-	_rm "$PREFIX/share/wallpapers/snow"
-	_rm "$PREFIX/share/wallpapers/split"
-	_rm "$PREFIX/share/wallpapers/sunset"
-	_rm "$PREFIX/share/wallpapers/svalbard"
-	_rm "$PREFIX/share/wallpapers/wave"
-	_rm "$PREFIX/share/wallpapers/wild"
-	_rm "$PREFIX/share/wallpapers/wirefeather"
-	_rm "$PREFIX/share/wallpapers/wireparts"
-}
-
-_install() {
-    _msg "Installing ..."
-    sudo cp -R \
-        "$temp_dir/$gh_repo-$TAG/plasma" \
-        "$temp_dir/$gh_repo-$TAG/wallpapers" \
-        "$PREFIX/share"
-}
-
-temp_file="$(mktemp -u)"
-temp_dir="$(mktemp -d)"
-
-if [ "$uninstall" = "false" ]; then
-    _uninstall
-    _install
+# Destination directory
+if [ "$UID" -eq "$ROOT_UID" ]; then
+  PLASMA_DIR="/usr/share/plasma/desktoptheme"
+  LOOKFEEL_DIR="/usr/share/plasma/look-and-feel"
+  WALLPAPER_DIR="/usr/share/wallpapers"
 else
-    _uninstall
+  PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
+  LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
+  WALLPAPER_DIR="$HOME/.local/share/wallpapers"
 fi
+
+THEME_NAME=Sunset
+
+[[ ! -d ${PLASMA_DIR} ]] && mkdir -p ${PLASMA_DIR}
+[[ ! -d ${LOOKFEEL_DIR} ]] && mkdir -p ${LOOKFEEL_DIR}
+[[ ! -d ${WALLPAPER_DIR} ]] && mkdir -p ${WALLPAPER_DIR}
+
+install() {
+  local name=${1}
+
+  cp -r ${SRC_DIR}/plasma/desktoptheme/*                                             ${PLASMA_DIR}
+  cp -r ${SRC_DIR}/plasma/look-and-feel/*                                            ${LOOKFEEL_DIR}
+  cp -r ${SRC_DIR}/wallpapers/*                                                       ${WALLPAPER_DIR}
+}
+
+echo "Installing '${THEME_NAME} kde themes'..."
+
+install "${name:-${THEME_NAME}}"
+
+echo "Install finished..."
