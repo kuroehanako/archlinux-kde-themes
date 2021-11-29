998010862f3cae1fd30892d2a4663c1b12d63b70
KuroeHanako <78864678+kuroehanako@users.noreply.github.com>
2021-11-29 21:39:00 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,25 +7,21 @@ ROOT_UID=0
 if [ "$UID" -eq "$ROOT_UID" ]; then
   PLASMA_DIR="/usr/share/plasma/desktoptheme"
   LOOKFEEL_DIR="/usr/share/plasma/look-and-feel"
-  WALLPAPER_DIR="/usr/share/wallpapers"
 else
   PLASMA_DIR="$HOME/.local/share/plasma/desktoptheme"
   LOOKFEEL_DIR="$HOME/.local/share/plasma/look-and-feel"
-  WALLPAPER_DIR="$HOME/.local/share/wallpapers"
 fi
 
 THEME_NAME=Sunset
 
 [[ ! -d ${PLASMA_DIR} ]] && mkdir -p ${PLASMA_DIR}
 [[ ! -d ${LOOKFEEL_DIR} ]] && mkdir -p ${LOOKFEEL_DIR}
-[[ ! -d ${WALLPAPER_DIR} ]] && mkdir -p ${WALLPAPER_DIR}
 
 install() {
   local name=${1}
 
   cp -r ${SRC_DIR}/plasma/desktoptheme/*                                             ${PLASMA_DIR}
   cp -r ${SRC_DIR}/plasma/look-and-feel/*                                            ${LOOKFEEL_DIR}
-  cp -r ${SRC_DIR}/wallpapers/*                                                       ${WALLPAPER_DIR}
 }
 
 echo "Installing '${THEME_NAME} kde themes'..."
